322fc5fda71d5e1c34f2723f5d597cfb9dbfab1d
veronnah <yoo.veroon@gmail.com>
2022-04-01 18:38:14 +0200

    user: home page(blog) cards design fixes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {PostsService} from "../shared/posts.service";
 import {Observable} from "rxjs";
 import {Post} from "../shared/interfaces";
@@ -6,7 +6,8 @@ import {Post} from "../shared/interfaces";
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomePageComponent implements OnInit {
 
