bdc4eb23f64cf1cc70e64f462eb1c7bb7fb61937
veronnah <yoo.veroon@gmail.com>
2022-04-05 18:55:00 +0200

    user: about us page started

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.scss']
+  styleUrls: ['./about-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AboutPageComponent implements OnInit {
 
